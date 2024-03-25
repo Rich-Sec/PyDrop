dbd6a404a9bb59e9f26e7791b36d21a5bdbf0d42
Richard Smith <richardsmithip@gmail.com>
2024-03-25 21:54:58 +0000

    Added support for SSL/TLS

--- main.py
+++ main.py
@@ -7,38 +7,95 @@ from client import Client
 import argparse
 
 def main():
+
+    pyDropImage = """                                                                                                    
+                                                 7!                                                 
+                                                !55!                                                
+                                               !5555!                                               
+                                              75555557                                              
+                                            .?55555555?.                                            
+                                           :J5555555555J:                                           
+                                          ~Y555555555555Y~                                          
+                                        .755555555555555557.                                        
+                                       :J555555555555555555J:                                       
+                                      ~Y55555555555555555555Y~                                      
+                                    .75555555555555555555555557.                                    
+                                   :J55555555555555555555555555J:                                   
+                                  ^Y5555555555555555555555555555Y^                                  
+                                 ~55555555555555555555555555555555~                                 
+                                !55YY555555555555555555555555555555!                                
+                               ~55Y!55555555555555555555555555555555~                               
+                              ^55Y:~555555555555555555555555555555555^                              
+                             .Y55~ 7555555555555555555555555555555555Y.                             
+                             ~55J  75555555555555555555555555555555555~                             
+                             7557  !55555555555555555555555555555555557                             
+                             !55?  :5555555555555555555555555555555555!                             
+                             :555:  ?555555555555555555555555555555555:                             
+                              755J. :Y55555555555555555555555555555557                              
+                               ?55J. ^555555555555555555555555555555?                               
+                                755Y~ :Y5555555555555555555555555557                                
+                                 ^J55J~:7Y55555555555555555555555J^                                 
+                                  .~J55Y7!?Y5555555555555555555J~.                                  
+                                     :!J555Y555555555555555YJ!:                                     
+                                        :^!?JYY555555YYJ?!^:                                        
+                                             ..::::::..                                             
+                                                                                                    
+                                                                                                    
+                                                                                                    """
+      
+    print(pyDropImage)
+
     parser = argparse.ArgumentParser(description='Transfer files using Python.')
     parser.add_argument('MODE', type=str, choices=['server','client'], help="Use PyDrop in Server or Client mode")
-    parser.add_argument('--filepath', '-FP', type=str, help="The directory containing files for access, for use in SERVER mode. By default PyDrop users the current working directory")
-    parser.add_argument('--filename', '-F', type=str, help="The filename to retrieve from the PyDrop server")
+    parser.add_argument('--filepath', '-fp', type=str, help="The directory containing files for access, for use in SERVER mode. By default PyDrop users the current working directory")
+    parser.add_argument('--filename', '-f', type=str, help="The filename to retrieve from the PyDrop server")
     parser.add_argument('--host', '-H', type=str, help="Use in CLIENT mode to specify the IPv4 address of the PyDrop server. By default, PyDrop Server listens on 0.0.0.0")
-    parser.add_argument('--port', '-P', type=int, help="The TCP port of the PyDrop Server, in SERVER mode PyDrop defaults to 9090/tcp")
+    parser.add_argument('--port', '-p', type=int, help="The TCP port of the PyDrop Server, in SERVER mode PyDrop defaults to 9090/tcp")
+    parser.add_argument('--encrypted', '-e', action='store_true', help="Use SSL/TLS encryption to transfer files")
+    parser.add_argument('--certfile','-c', type=str, help="If --encryption/-e is specified in SERVER mode, a certfile must be specified")
+    parser.add_argument('--keyfile','-k', type=str, help="If --encryption/-e is specified in SERVER mode, a keyfile must be specified")
     args = parser.parse_args()
 
-    # Instantiate a new server object:
-    if (args.MODE == "server"):
-        # Port and Filepath are user defined:
-        if (args.port is not None and args.filepath is not None):
-            PyDropServer = Server(PORT=args.port, FILEPATH=args.filename)
-        # Filepath is user defined:
-        elif(args.port is not None and args.filepath is None):
-            PyDropServer = Server(PORT=args.port)
-        # Port is user defined:
-        elif(args.port is None and args.filepath is not None):
-            PyDropServer = Server(FILEPATH=args.filepath)
-        # Nothing is user defined:
+    # Instantiate a new server (encrypted) object:
+    if (args.MODE == "server" and args.encrypted is True):
+        # Check certfiles and keyfiles:
+        if (args.certfile is None or args.keyfile is None):
+            parser.print_help()
+            exit()
         else:
-            PyDropServer = Server()
-        PyDropServer.unencryptedServer()
+            if (args.port is not None and args.filepath is not None):
+                PyDropServer = Server(PORT=args.port, FILEPATH=args.filename, CERTFILE=args.certfile, KEYFILE=args.keyfile) # Port and filename are user defined
+            elif(args.port is not None and args.filepath is None):
+                PyDropServer = Server(PORT=args.port, CERTFILE=args.certfile, KEYFILE=args.keyfile) # Port is user defined
+            elif(args.port is None and args.filepath is not None):
+                PyDropServer = Server(FILEPATH=args.filepath, CERTFILE=args.certfile, KEYFILE=args.keyfile) # Filepath is user defined
+            else:
+                PyDropServer = Server(CERTFILE=args.certfile, KEYFILE=args.keyfile) # Only neccessary encryption files are specified
+        PyDropServer.encrypted_server()
+    
+    # Instantiate a new server (unencrypted) object:
+    elif (args.MODE == "server" and args.encrypted is False):
+        if (args.port is not None and args.filepath is not None):
+            PyDropServer = Server(PORT=args.port, FILEPATH=args.filename) # Port and Filepath are user defined:
+        elif(args.port is not None and args.filepath is None):
+            PyDropServer = Server(PORT=args.port) # Filepath is user defined:
+        elif(args.port is None and args.filepath is not None):
+            PyDropServer = Server(FILEPATH=args.filepath) # Port is user defined:
+        else:
+            PyDropServer = Server() # Nothing is user defined:
+        PyDropServer.unencrypted_server()
+
     # Instantiate a new client object:
-    elif (args.MODE == "client"):
-        # User must specify all neccessary parameters in client mode:
-        if (args.host is None or args.port is None or args.filename is None):
-            print(f"In CLIENT mode the following flags must be specified:\n*--filename/-f\n*--host/-h\n*--port/-p")
+    elif (args.MODE == "client" and args.encrypted is False):
+        if (args.host is None or args.port is None or args.filename is None): # User must specify all neccessary parameters in client mode:
+            parser.print_help()
         else:
             PyDropClient = Client(args.host, args.port, args.filename)
-            PyDropClient.fetchFile()
-    else:
-        pass
-
+            PyDropClient.fetch_file()
+    elif (args.MODE == "client" and args.encrypted is True):
+        if (args.host is None or args.port is None or args.filename is None): # User must specify all neccessary parameters in client mode:
+            parser.print_help()
+        else:
+            PyDropClient = Client(args.host, args.port, args.filename)
+            PyDropClient.secure_fetch_file()
 main()
--- server.py
+++ server.py
@@ -8,24 +8,26 @@ import signal
 import select
 
 class Server:
-    def __init__(self, HOST="0.0.0.0", PORT=9090, FILEPATH="./", *args, **kwargs):
+    def __init__(self, HOST="0.0.0.0", PORT=9090, FILEPATH="./", CERTFILE=None, KEYFILE=None, *args, **kwargs):
         self.HOST = HOST
         self.PORT = PORT
         self.FILEPATH = FILEPATH # Default to current directory
         self.SERVER_SOCKET = None
         self.ALIVE = False
+        self.CERTFILE= CERTFILE
+        self.KEYFILE = KEYFILE
     
-    def unencryptedServer(self):
-        # Create server listener and await incoming connection:
-        
+    def unencrypted_server(self):
+        # Register signal handler for SIGINT (CTRL+C):
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        # Create server socket:
         self.SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.SERVER_SOCKET.bind((self.HOST, self.PORT))
         self.SERVER_SOCKET.listen()
         self.ALIVE = True
-        print(f"PyDrop Listening on {self.HOST}:{self.PORT}")
 
-        # Register signal handler for SIGINT (CTRL+C)
-        signal.signal(signal.SIGINT, self.signal_handler)
+        print(f"PyDrop Listening on {self.HOST}:{self.PORT}")
 
         # Accept incoming connection from client and send file data:
         while self.ALIVE:
@@ -67,6 +69,64 @@ class Server:
             except KeyboardInterrupt:
                 print("Stopping PyDrop Server...")
                 self.stop()
+
+    def encrypted_server(self):
+        # Register signal handler for SIGINT (CTRL+C)
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        # Create server socket
+        self.SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.SERVER_SOCKET.bind((self.HOST, self.PORT))
+        self.SERVER_SOCKET.listen()
+
+        # Wrap server socket with SSL/TLS context
+        ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(certfile=self.CERTFILE, keyfile=self.KEYFILE)
+        
+        with ssl_context.wrap_socket(self.SERVER_SOCKET, server_side=True) as ssl_socket:
+            self.ALIVE = True
+            print(f"PyDrop Listening on {self.HOST}:{self.PORT}")
+
+            # Accept incoming connection from client and send file data:
+            while self.ALIVE:
+                try:
+                    # Used to wait for connection with timeout
+                    readable, _, _ = select.select([ssl_socket], [], [], 1.0)
+                    if readable:
+                        conn, addr = ssl_socket.accept()
+                        try:
+                            with conn:
+                                print(f"Client Connection {addr} Initiated")
+
+                                # Read filename specified by the client
+                                filenameLengthBytes = conn.recv(4)
+                                filenameLength = int.from_bytes(filenameLengthBytes, byteorder="big")
+                                filename = conn.recv(filenameLength).decode()
+                                
+                                # Check if file exists
+                                try:
+                                    # Open the file and parse the contents
+                                    with open(filename, "rb") as file:
+                                        fileData = file.read()
+                                except:
+                                    print(f"Unable to access file {filename}")
+                                    continue
+                                try:
+                                    # Send filename length and data to client:
+                                    conn.sendall(len(filename).to_bytes(4, byteorder="big"))
+                                    conn.sendall(filename.encode())
+                                    # Send file data length and file data to client:
+                                    conn.sendall(len(fileData).to_bytes(8, byteorder="big"))
+                                    conn.sendall(fileData)
+
+                                    print(f"File {filename} Sent Successfully")
+                                except Exception as e:
+                                    print(e)
+                        except Exception as e:
+                            print(e)
+                except KeyboardInterrupt:
+                    print("Stopping TLS Server...")
+                    self.stop()
 
     def signal_handler(self, sig, frame):
         print("Caught CTRL+C, Stopping PyDrop Server...")
